18141d8b3547bb456e201a62f6ef12935188d078
flipskatehd <flipskatehd@gmail.com>
2025-10-05 22:04:58 -0300

    fazendo tela inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+
+  <head>
+    <title>Escola SAEP</title>
+  </head>
+  
+  <body>
+    <h1> Sistema de gerenciamento de itens escolares - SAEP</h1>
+
+    <a href="login.php">Login</a> <br> </br>
+    <a href="cadastroUsuario.php">Cadastrar-se</a>
+  </body>
+</html>
