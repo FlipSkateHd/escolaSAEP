d122e8097ec03cf0bd16894af74b8a571df89e16
flipskatehd <flipskatehd@gmail.com>
2025-10-07 12:45:00 -0300

    erro no código reparado.

--- inicio.php
+++ inicio.php
@@ -8,37 +8,16 @@
 
 
   <?php
-  /** Início do código 
-   */
   session_start();
   if (empty($_SESSION['idUser'])) {
     echo '<script>prompt("por favor, efetue o login")</script>';
     header('Location: index.php');
   } else {
     echo '<h1>bem vindo!<h1>';
-  }
-  ?>
+  } ?>
 
 
-</html>
-</html>    } else {
-          echo'<h1>bem vindo!<h1>';
-    }
-    ?>
+</body>
 
 
-  </body>
-</html>
-    session_start();
-    if (empty($_SESSION['idUser']) ) {
-          echo'<script>prompt("por favor, efetue o login")</script>';
-          header('Location: index.php');
-
-    } else {
-          echo'<h1>bem vindo!<h1>';
-    }
-    ?>
-
-
-  </body>
 </html>
